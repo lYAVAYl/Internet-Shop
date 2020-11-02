bdcb97bb4f403ad4230d7eed3e3408a169009979
Dmitrii Shorokhov <jo.eng@yandex.ru>
2020-11-02 08:14:10 +0300

    Add project files.

---
+++ Shop.Application/ProductsStock/GetStock.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+
+namespace Shop.Application.StockAdmin
+{
+    public class GetStock
+    {
+        private ApplicationDbContext _ctx;
+
+        public GetStock(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<ProductViewModel> Do()
+        {
+            var stock = _ctx.Products
+                .Include(x=>x.Stock)
+                .Select(x => new ProductViewModel()
+                {
+                    Id = x.Id,
+                    Description = x.Description,
+                    Stock = x.Stock.Select(y =>
+                        new StockViewModel()
+                        {
+                            Id = y.Id,
+                            Descriprion = y.Descriprion,
+                            Qty = y.Qty
+                        })
+                })
+                .ToList();
+
+            return stock;
+        }
+
+        public class StockViewModel
+        {
+            public int Id { get; set; }
+            public string Descriprion { get; set; }
+            public int Qty { get; set; }
+            public int ProductId { get; set; }
+        }
+
+        public class ProductViewModel
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public IEnumerable<StockViewModel> Stock { get; set; }
+        }
+        
+    }
+}
---
+++ Shop.UI/wwwroot/js/admin/stock.js
@@ -0,0 +1,52 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        products: [],
+        selectedProduct: null,
+        newStock: {
+            productId: 0,
+            description: "Size",
+            qty: 1.55
+        }
+    },
+    mounted() {
+        this.getStock();
+    },
+    mewthods: {
+        getStock() {
+            this.loading = true;
+            axios.get('/Admin/stock')
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data;
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        updateStock() {
+
+        },
+        addStock() {
+            this.loading = true;
+            axios.get('/Admin/stocks/', this.newStock)
+                .then(res => {
+                    console.log(res);
+                    this.products.stock.push(res.data);
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+                .then(() => {
+                    this.loading = false;
+                });
+        },
+        selectProduct(product) {
+            this.selectedProduct = product;
+            this.newStock.productId = product.Id;
+        }
+    }
+})
